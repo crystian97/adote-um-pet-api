c3ba4cbdfed4465e956ad8d365d2ad08c52545be
crystian97 <icrystianveloso@gmail.com>
2022-06-06 11:07:36 -0300

    Migration

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -7,4 +7,29 @@ use Illuminate\Http\Request;
 class PetController extends Controller
 {
     //
+    public function index(){
+
+        return [
+            [
+            'nome'=>'cachorro',
+            'raca'=>'vira-lata',
+            'idade'=>'2 anos',
+            'peso'=>'10kg'
+            ],
+            [
+                'nome'=>'cachorro2',
+                'raca'=>'vira-lata',
+                'idade'=>'2 anos',
+                'peso'=>'10kg'
+            ],
+            [
+                'nome'=>'cachorro3',
+                'raca'=>'vira-lata',
+                'idade'=>'2 anos',
+                'peso'=>'10kg'
+            ]
+            ];
+
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/pets',function(){
-
-    echo 'estou na lista de pets';
-});
+Route::get('/pets',[PetController::class,'index']);
 
 Route::post('/pets',function(){
 
